cb325c61377ea7b8d9af1a3b1c66b067693a3518
Connie Leung <cleung334@gmail.com>
2024-09-10 00:28:26 +0800

    chore: install langchain dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StorytellingModule } from './storytelling/storytelling.module';
 
 @Module({
-  imports: [],
+  imports: [StorytellingModule],
   controllers: [AppController],
   providers: [AppService],
 })
