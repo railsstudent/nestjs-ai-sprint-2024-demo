dc80e1d4a13b28cfa53bb61bf56797e4f0e9b926
Connie Leung <cleung334@gmail.com>
2024-09-10 01:15:02 +0800

    feat: provide Groq chat model

---
+++ src/storytelling/application/providers/model.provider.ts
@@ -0,0 +1,24 @@
+import { ChatGroq } from '@langchain/groq';
+import { Inject, Provider } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GROQ_CHAT_MODEL } from '../constants/groq.constant';
+import { GroqConfig } from '../types/groq-config.type';
+
+export function InjectModel() {
+  return Inject(GROQ_CHAT_MODEL);
+}
+
+export const GroqChatModelProvider: Provider<ChatGroq> = {
+  provide: GROQ_CHAT_MODEL,
+  useFactory: (configService: ConfigService) => {
+    const { apiKey, model } = configService.get<GroqConfig>('groq');
+    return new ChatGroq({
+      apiKey,
+      model,
+      temperature: 0.7,
+      maxTokens: 2048,
+      streaming: false,
+    });
+  },
+  inject: [ConfigService],
+};
--- src/storytelling/storytelling.module.ts
+++ src/storytelling/storytelling.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { StorytellingController } from './presenters/http/storytelling.controller';
+import { GroqChatModelProvider } from './application/providers/groq-chat-model.provider';
 import { StorytellingService } from './application/storytelling.service';
+import { StorytellingController } from './presenters/http/storytelling.controller';
 
 @Module({
   controllers: [StorytellingController],
-  providers: [StorytellingService],
+  providers: [StorytellingService, GroqChatModelProvider],
 })
 export class StorytellingModule {}
